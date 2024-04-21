1dbd3cabeeb43ac6674f043686823b1d2068c39c
llyriq <kir.kon2018@gmail.com>
2024-04-21 15:28:52 +0300

    Создана логика для считывания месячных отчётов

--- src/main/java/org/llyriq/Main.java
+++ src/main/java/org/llyriq/Main.java
@@ -1,24 +1,81 @@
 package org.llyriq;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
-
+    static MonthlyReport monthlyReport = new MonthlyReport();
+    static String resourcesPath = "src/main/resources";
+    static File resources = new File(resourcesPath);
 
 
 
     public static void main(String[] args) {
+        label:
         while(true){
-            if(true){
-                doFirst();
-            } else {
-                break;
+            System.out.println("Выберите действие:");
+            System.out.println("1. Считать все месячные отчёты");
+            System.out.println("2. Считать годовой отчёт");
+            System.out.println("3. Сверить отчёты");
+            System.out.println("4. Вывести информацию о всех месячных отчётах");
+            System.out.println("5. Вывести информацию о годовом отчёте");
+            System.out.println("Для выхода введите \"stop\".\n");
+            String choice = scanner.next();
+            switch (choice) {
+                case "1":
+                    readMonths();
+                    break;
+                case "2":
+                    readYear();
+                    break;
+                case "3":
+                    inspectReports();
+                    break;
+                case "4":
+                    mountReportsInfo();
+                    break;
+                case "5":
+                    yearReportInfo();
+                    break;
+                case "stop":
+                    break label;
+                default:
+                    System.out.println("Введена неверная команда, пожалуйста повторите ввод.\n");
+                    break;
             }
         }
     }
 
-    public static void doFirst(){
+    public static void readMonths() {
+        String[] reports = resources.list();
+
+        assert reports != null;
+        for (String report : reports) {
+            if(report.charAt(0) == 'm') {
+                monthlyReport.readMonth(resourcesPath, report);
+            }
+        }
+
+        System.out.println("Все месячные отчёты загружены. Для продолжения нажмите \"Enter\".");
+        scanner.nextLine();
+        scanner.nextLine();
+
+    }
+
+    public static void readYear(){
+
+    }
+
+    public static void inspectReports(){
+
+    }
+
+    public static void mountReportsInfo(){
+
+    }
+
+    public static void yearReportInfo(){
 
     }
 }
--- src/main/java/org/llyriq/MonthlyReport.java
+++ src/main/java/org/llyriq/MonthlyReport.java
@@ -1,7 +1,11 @@
 package org.llyriq;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MonthlyReport {
     HashMap<Integer, ArrayList<MonthData>> data = new HashMap<>();
@@ -13,7 +17,33 @@ public class MonthlyReport {
         }
     }
 
-    public void addMonth(int month, ArrayList<MonthData> monthData){
+    public void replaceMonth(int month, ArrayList<MonthData> monthData){
         data.replace(month, monthData);
+    }
+
+    public void readMonth(String path, String report){
+        try {
+            List<String> lines = Files.readAllLines(Path.of(path + "/" + report)); //Считываются строки из файла
+            ArrayList<MonthData> monthData = new ArrayList<>();
+            boolean isHeader = true;
+            for(String line : lines) {
+                if(!isHeader) {//проверка на заголовок
+                    String[] lineContents = line.split(",");//строка разбивается на параметры
+
+                    monthData.add(new MonthData(lineContents[0].trim(),//создается экземпляр класса с заполнением параметров
+                            Boolean.parseBoolean(lineContents[1].trim()),//которые предварительно очищаются от пробелов
+                            Integer.parseInt(lineContents[2].trim()),
+                            Integer.parseInt(lineContents[3].trim())));
+                } else { isHeader = false; }
+            }
+            int numberMonth = Integer.parseInt(report.substring(6,8)); //вычисляется номер месяца путём обрезки имени файла
+            replaceMonth(numberMonth, monthData);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void showData(){
+        System.out.println(data);
     }
 }
